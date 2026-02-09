0ba02db16992069c3fecee4d89df388b1d5a2018
Mario6002 <benavidezmario084@gmail.com>
2026-02-09 13:13:36 -0500

    Update Snake.ts

--- src/Snake.ts
+++ src/Snake.ts
@@ -1,34 +1,75 @@
+import { Point } from "./Point";
+
+/**
+ * Represents a snake in a 2D grid.
+ */
 export class Snake {
-    // private attributes
-    private currentPosition: number;
-    private currentDirection: number;
-  
-    constructor() {
-      // starts at position 0, facing forward
-      this.currentPosition = 0;
-      this.currentDirection = 1;
-    }
-  
-    // move the snake
-    move(squares: number): void {
-      if (this.currentDirection === 1) {
-        this.currentPosition += squares;
-      } else {
-        this.currentPosition -= squares;
-      }
-    }
-  
-    // turn the snake around
-    turn(): void {
-      if (this.currentDirection === 1) {
-        this.currentDirection = -1;
-      } else {
-        this.currentDirection = 1;
-      }
-    }
-  
-    // getter for position
-    get position(): number {
-      return this.currentPosition;
+  private position: Point;
+  private direction: "up" | "down" | "left" | "right";
+
+  /**
+   * Creates a new Snake at (0,0) facing right.
+   */
+  constructor() {
+    this.position = new Point(0, 0);
+    this.direction = "right";
+  }
+
+  /**
+   * Gets the snake's current position.
+   */
+  getPosition(): Point {
+    return this.position;
+  }
+
+  /**
+   * Gets the snake's current direction.
+   */
+  getDirection(): string {
+    return this.direction;
+  }
+
+  /**
+   * Moves the snake forward.
+   * @param squares Number of squares to move
+   */
+  move(squares: number): void {
+    if (this.direction === "right") {
+      this.position = new Point(this.position.x + squares, this.position.y);
+    } else if (this.direction === "left") {
+      this.position = new Point(this.position.x - squares, this.position.y);
+    } else if (this.direction === "up") {
+      this.position = new Point(this.position.x, this.position.y + squares);
+    } else if (this.direction === "down") {
+      this.position = new Point(this.position.x, this.position.y - squares);
     }
   }
+
+  /**
+   * Turns the snake left.
+   */
+  turnLeft(): void {
+    if (this.direction === "up") this.direction = "left";
+    else if (this.direction === "left") this.direction = "down";
+    else if (this.direction === "down") this.direction = "right";
+    else if (this.direction === "right") this.direction = "up";
+  }
+
+  /**
+   * Turns the snake right.
+   */
+  turnRight(): void {
+    if (this.direction === "up") this.direction = "right";
+    else if (this.direction === "right") this.direction = "down";
+    else if (this.direction === "down") this.direction = "left";
+    else if (this.direction === "left") this.direction = "up";
+  }
+
+  /**
+   * Turns the snake (deprecated).
+   * @deprecated Use turnLeft() or turnRight() instead.
+   */
+  turn(): void {
+    this.turnRight();
+  }
+}
