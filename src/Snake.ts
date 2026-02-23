64c403d2f57c75946c01491370b8d6fcd1aa4ed0
Mario6002 <benavidezmario084@gmail.com>
2026-02-23 15:21:33 -0500

    Update Snake.ts

--- src/Snake.ts
+++ src/Snake.ts
@@ -1,75 +1,76 @@
-import { Point } from "./Point";
+ import { Point } from './point';
 
 /**
- * Represents a snake in a 2D grid.
+ * Represents a snake in the game
  */
 export class Snake {
-  private position: Point;
-  private direction: "up" | "down" | "left" | "right";
+    private _position: Point[];
+    private _direction: string;
+    private readonly directions: string[] = ['up', 'right', 'down', 'left'];
 
-  /**
-   * Creates a new Snake at (0,0) facing right.
-   */
-  constructor() {
-    this.position = new Point(0, 0);
-    this.direction = "right";
-  }
-
-  /**
-   * Gets the snake's current position.
-   */
-  getPosition(): Point {
-    return this.position;
-  }
-
-  /**
-   * Gets the snake's current direction.
-   */
-  getDirection(): string {
-    return this.direction;
-  }
-
-  /**
-   * Moves the snake forward.
-   * @param squares Number of squares to move
-   */
-  move(squares: number): void {
-    if (this.direction === "right") {
-      this.position = new Point(this.position.x + squares, this.position.y);
-    } else if (this.direction === "left") {
-      this.position = new Point(this.position.x - squares, this.position.y);
-    } else if (this.direction === "up") {
-      this.position = new Point(this.position.x, this.position.y + squares);
-    } else if (this.direction === "down") {
-      this.position = new Point(this.position.x, this.position.y - squares);
+    /**
+     * Creates a new Snake
+     * @param position - Initial position points of the snake
+     */
+    constructor(position: Point[]) {
+        this._position = [...position];
+        this._direction = 'right';
     }
-  }
 
-  /**
-   * Turns the snake left.
-   */
-  turnLeft(): void {
-    if (this.direction === "up") this.direction = "left";
-    else if (this.direction === "left") this.direction = "down";
-    else if (this.direction === "down") this.direction = "right";
-    else if (this.direction === "right") this.direction = "up";
-  }
+    /**
+     * Gets the snake's position
+     */
+    get position(): Point[] {
+        return [...this._position];
+    }
 
-  /**
-   * Turns the snake right.
-   */
-  turnRight(): void {
-    if (this.direction === "up") this.direction = "right";
-    else if (this.direction === "right") this.direction = "down";
-    else if (this.direction === "down") this.direction = "left";
-    else if (this.direction === "left") this.direction = "up";
-  }
+    /**
+     * Gets the snake's current direction
+     */
+    get direction(): string {
+        return this._direction;
+    }
 
-  /**
-   * Turns the snake (deprecated).
-   * @deprecated Use turnLeft() or turnRight() instead.
-   */
-  turn(): void {
-    this.turnRight();
-  }
+    /**
+     * Moves the snake forward one step
+     */
+    move(): void {
+        const head = this._position[0];
+        let newHead: Point;
+
+        switch (this._direction) {
+            case 'up':
+                newHead = new Point(head.x, head.y - 1);
+                break;
+            case 'down':
+                newHead = new Point(head.x, head.y + 1);
+                break;
+            case 'left':
+                newHead = new Point(head.x - 1, head.y);
+                break;
+            case 'right':
+                newHead = new Point(head.x + 1, head.y);
+                break;
+            default:
+                newHead = head;
+        }
+
+        this._position = [newHead, ...this._position.slice(0, -1)];
+    }
+
+    /**
+     * Turns the snake left
+     */
+    turnLeft(): void {
+        const currentIndex = this.directions.indexOf(this._direction);
+        this._direction = this.directions[(currentIndex + 3) % 4];
+    }
+
+    /**
+     * Turns the snake right
+     */
+    turnRight(): void {
+        const currentIndex = this.directions.indexOf(this._direction);
+        this._direction = this.directions[(currentIndex + 1) % 4];
+    }
 }
