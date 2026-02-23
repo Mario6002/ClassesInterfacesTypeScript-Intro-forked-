b2e2fb9e3f4c4094a3a7f505b7644b898d26ea8e
Mario6002 <benavidezmario084@gmail.com>
2026-02-23 15:32:47 -0500

    Update WorldModel.ts

--- src/WorldModel.ts
+++ src/WorldModel.ts
@@ -1,31 +1,50 @@
-import { Snake } from "./Snake";
+import { Snake } from './snake';
 
 /**
- * Represents the world model containing a snake.
+ * Represents the game world containing all snakes
  */
 export class WorldModel {
-  private snake: Snake;
+    private _width: number;
+    private _height: number;
+    private _snakes: Snake[];
 
-  /**
-   * Creates a WorldModel with an existing Snake.
-   * @param snake The snake in the world
-   */
-  constructor(snake: Snake) {
-    this.snake = snake;
-  }
+    /**
+     * Creates a new WorldModel
+     * @param width - Width of the world
+     * @param height - Height of the world
+     * @param snakes - Initial snakes in the world
+     */
+    constructor(width: number, height: number, snakes: Snake[] = []) {
+        this._width = width;
+        this._height = height;
+        this._snakes = snakes;
+    }
 
-  /**
-   * Updates the world by moving the snake.
-   * @param steps Number of steps to move
-   */
-  update(steps: number): void {
-    this.snake.move(steps);
-  }
+    /**
+     * Gets the world width
+     */
+    get width(): number {
+        return this._width;
+    }
 
-  /**
-   * Gets the snake.
-   */
-  getSnake(): Snake {
-    return this.snake;
-  }
+    /**
+     * Gets the world height
+     */
+    get height(): number {
+        return this._height;
+    }
+
+    /**
+     * Gets all snakes in the world
+     */
+    get snakes(): Snake[] {
+        return [...this._snakes];
+    }
+
+    /**
+     * Updates the world state
+     */
+    update(): void {
+        this._snakes.forEach(snake => snake.move());
+    }
 }
