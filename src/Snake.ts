e5fae9a75b72e03e21a2d57a630f2eb1ef381ac9
Mario Benavidez <benavidezmario084@gmail.com>
2026-03-26 14:18:12 -0400

    Update Snake.ts

--- src/Snake.ts
+++ src/Snake.ts
@@ -1,76 +1,173 @@
- import { Point } from './point';
+import { Point } from './Point';
+import { Direction } from './Direction';
 
 /**
- * Represents a snake in the game
+ * Represents a snake in the game with multiple body parts
  */
 export class Snake {
-    private _position: Point[];
-    private _direction: string;
-    private readonly directions: string[] = ['up', 'right', 'down', 'left'];
+    /** Array of points representing each part of the snake (head first, tail last) */
+    private currentParts: Point[];
+    
+    /** Current movement direction of the snake */
+    private direction: Direction;
+    
+    /** Initial position of the snake's head */
+    private startPosition: Point;
+    
+    /** Length of the snake (number of parts) */
+    private size: number;
 
     /**
      * Creates a new Snake
-     * @param position - Initial position points of the snake
+     * @param startPosition - Initial head position
+     * @param size - Initial length of the snake (minimum 1)
      */
-    constructor(position: Point[]) {
-        this._position = [...position];
-        this._direction = 'right';
-    }
-
-    /**
-     * Gets the snake's position
-     */
-    get position(): Point[] {
-        return [...this._position];
-    }
-
-    /**
-     * Gets the snake's current direction
-     */
-    get direction(): string {
-        return this._direction;
-    }
-
-    /**
-     * Moves the snake forward one step
-     */
-    move(): void {
-        const head = this._position[0];
-        let newHead: Point;
-
-        switch (this._direction) {
-            case 'up':
-                newHead = new Point(head.x, head.y - 1);
-                break;
-            case 'down':
-                newHead = new Point(head.x, head.y + 1);
-                break;
-            case 'left':
-                newHead = new Point(head.x - 1, head.y);
-                break;
-            case 'right':
-                newHead = new Point(head.x + 1, head.y);
-                break;
-            default:
-                newHead = head;
+    constructor(startPosition: Point, size: number) {
+        this.startPosition = startPosition;
+        this.size = size;
+        this.currentParts = [];
+        this.direction = Direction.Right;
+        
+        // Initialize snake parts
+        // Start with head at startPosition
+        this.currentParts.push(new Point(startPosition.x, startPosition.y));
+        
+        // Add tail parts extending to the left (assuming initial direction is Right)
+        for (let i = 1; i < size; i++) {
+            this.currentParts.push(new Point(startPosition.x - i, startPosition.y));
         }
+    }
 
-        this._position = [newHead, ...this._position.slice(0, -1)];
+    /**
+     * Gets the current position of the snake's head
+     * @returns The head position
+     */
+    getHeadPosition(): Point {
+        return this.currentParts[0];
+    }
+
+    /**
+     * Gets all parts of the snake
+     * @returns Array of points representing snake parts
+     */
+    getAllParts(): Point[] {
+        return [...this.currentParts];
+    }
+
+    /**
+     * Gets the current movement direction
+     * @returns Current direction
+     */
+    getDirection(): Direction {
+        return this.direction;
     }
 
     /**
      * Turns the snake left
      */
     turnLeft(): void {
-        const currentIndex = this.directions.indexOf(this._direction);
-        this._direction = this.directions[(currentIndex + 3) % 4];
+        switch (this.direction) {
+            case Direction.Up:
+                this.direction = Direction.Left;
+                break;
+            case Direction.Down:
+                this.direction = Direction.Right;
+                break;
+            case Direction.Left:
+                this.direction = Direction.Down;
+                break;
+            case Direction.Right:
+                this.direction = Direction.Up;
+                break;
+        }
     }
 
     /**
      * Turns the snake right
      */
     turnRight(): void {
-        const currentIndex = this.directions.indexOf(this._direction);
-        this._direction = this.directions[(currentIndex + 1) % 4];
+        switch (this.direction) {
+            case Direction.Up:
+                this.direction = Direction.Right;
+                break;
+            case Direction.Down:
+                this.direction = Direction.Left;
+                break;
+            case Direction.Left:
+                this.direction = Direction.Up;
+                break;
+            case Direction.Right:
+                this.direction = Direction.Down;
+                break;
+        }
+    }
+
+    /**
+     * Moves the snake forward
+     * Updates all body parts to follow the head
+     */
+    move(): void {
+        // Move tail parts forward (from back to front)
+        for (let i = this.currentParts.length - 1; i > 0; i--) {
+            this.currentParts[i] = new Point(this.currentParts[i - 1].x, this.currentParts[i - 1].y);
+        }
+        
+        // Move head based on current direction
+        const head = this.currentParts[0];
+        let newHead: Point;
+        
+        switch (this.direction) {
+            case Direction.Up:
+                newHead = new Point(head.x, head.y - 1);
+                break;
+            case Direction.Down:
+                newHead = new Point(head.x, head.y + 1);
+                break;
+            case Direction.Left:
+                newHead = new Point(head.x - 1, head.y);
+                break;
+            case Direction.Right:
+                newHead = new Point(head.x + 1, head.y);
+                break;
+        }
+        
+        this.currentParts[0] = newHead;
+    }
+
+    /**
+     * Checks if this snake collides with another snake
+     * @param otherSnake - The snake to check collision with
+     * @returns True if this snake's head collides with any part of the other snake
+     */
+    didCollide(otherSnake: Snake): boolean {
+        const head = this.getHeadPosition();
+        
+        // Check collision with all parts of the other snake
+        const otherParts = otherSnake.getAllParts();
+        
+        for (const part of otherParts) {
+            if (head.equals(part)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    /**
+     * Checks if this snake collides with itself
+     * @returns True if head collides with any tail part
+     */
+    didCollideWithSelf(): boolean {
+        const head = this.getHeadPosition();
+        const tailParts = this.currentParts.slice(1); // All parts except head
+        
+        for (const part of tailParts) {
+            if (head.equals(part)) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
